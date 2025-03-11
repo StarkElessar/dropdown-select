6df7abc3258c2dbf4d0db4583722d184ca61fa12
stark <serj.elessar@gmail.com>
2025-03-12 01:01:05 +0300

    refactor: code style

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,5 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-    base: '/dropdown-select/', // Имя вашего репозитория
-    build: {
-        outDir: 'dist'
-    }
+    base: '/dropdown-select/',
 }); 
